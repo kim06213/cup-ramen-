68ccd792d414cbeee189f9f5c5e24ef62505c2ce
kim06213 <62277299+kim06213@users.noreply.github.com>
2020-03-25 14:46:36 +0900

    Update MenuManager.java

--- MenuManager.java
+++ MenuManager.java
@@ -1,1 +1,13 @@
+public class MenuManager {
+	public static void main(String[] args) {
+		System.out.println("***Cup-Ramen Manager***");
+		System.out.println("Select one number between 1-5");
+		System.out.println("1. Add New Cup-Ramen");
+		System.out.println("2. Delete Cup-Ramen");
+		System.out.println("3. Edit Cup-Ramen");
+		System.out.println("4. View Cup-Ramen");
+		System.out.println("5. Exit");
+		}
+	}
+
 
